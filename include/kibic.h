0f3c1c3bf27f93cee4ad0137894fac9f5cfa1671
S3VIC <62557969+S3VIC@users.noreply.github.com>
2022-08-16 13:17:01 +0200

    Add files via upload
    Dodane funkcje wyznaczające zadowolenia kibiców i miasta

--- include/kibic.h
+++ include/kibic.h
@@ -6,12 +6,16 @@ class Kibic
   private:
     char kolor;
     double X, Y;
+    int zadowolenie;
   public:
     Kibic();
     Kibic(const char _kolor);
     const double getX();
     const double getY();
     const char getKolor();
+    void zwiekszZadowolenie();
+    void zmniejszZadowolenie();
+    int getZadowolenie();
     virtual ~Kibic();
 };
 
--- include/miasto.h
+++ include/miasto.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include "kibic.h"
+#include <cmath>
 
 class Miasto
 {
@@ -10,12 +11,17 @@ class Miasto
     double procent;
     std::string nazwaMiasta;
     std::vector<Kibic> kibice;
+    int zadowolenieMiasta;
 
   public:
     Miasto(std::string _nazwaMiasta);
     Miasto(int _N, int _p, std::string _nazwaMiasta);
     void zaludnijMiasto();
     void getPozycjeKibicow();
+    void wyznaczCalkZadowKibicow();
+    void wyznaczZadowKibica(Kibic& k1, Kibic& k2);
+    void wyznaczZadowMiasta();
+    int getZadowolenieMiasta();
     virtual ~Miasto();
 };
 
