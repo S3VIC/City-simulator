794def6138604f6a5700243b471b6434d4253d59
S3VIC <62557969+S3VIC@users.noreply.github.com>
2022-08-18 15:35:25 +0200

    Add files via upload
    Pierwszy opis klasy interface

---
+++ src/interface.cpp
@@ -0,0 +1,35 @@
+#include "interface.h"
+
+Interface::Interface(){}
+void Interface::inicjalizuj(){
+//  this->ustalWartParametrow()
+}
+void Interface::ustalWartParametrow(std::string* nazwaMiasta, int* liczbaMieszkancow, int* procent, int* liczbaEwolucji){
+  this->komunikatNazwaMiasta();
+  std::cin >> *nazwaMiasta;
+
+  this->komunikatLiczbaMieszk();
+  scanf("%d", liczbaMieszkancow);
+
+  this->komunikatProcentCzerw();
+  scanf("%d", procent);
+
+  this->komunikatLiczbaEwol();
+  scanf("%d", liczbaEwolucji);
+}
+
+void Interface::komunikatLiczbaEwol(){
+    printf("Liczba ewolucji: ");
+}
+
+void Interface::komunikatProcentCzerw(){
+  printf("Procent mieszkańców jaki stanowią kibice czerwoni: ");
+}
+
+void Interface::komunikatLiczbaMieszk(){
+  printf("Liczba mieszkańców: ");
+}
+
+void Interface::komunikatNazwaMiasta(){
+  printf("Nazwa miasta: ");
+}
