afc4707fd47833fd4a4749f649d6ffcbbe4da601
S3VIC <62557969+S3VIC@users.noreply.github.com>
2022-08-18 15:36:39 +0200

    Add files via upload
    Stworzenie klasy interface w celu obslugi wyswietlania komunikatow, a takze nadawania wartosci parametrom wejsciowym do symulatora

---
+++ include/interface.h
@@ -0,0 +1,15 @@
+#pragma once
+#include<iostream>
+
+class Interface{
+  public:
+  Interface();
+  void inicjalizuj();
+  void ustalWartParametrow(std::string* nazwaMiasta, int* liczbaMieszkancow, int* procent, int* liczbaEwolucji);
+  void komunikatNazwaMiasta();
+  void komunikatLiczbaMieszk();
+  void komunikatProcentCzerw();
+  void komunikatLiczbaEwol();
+
+  ~Interface();
+};
