af49af6b20c1acf2a3df53f2b7305faac2d52f43
S3VIC <62557969+S3VIC@users.noreply.github.com>
2022-08-18 14:36:21 +0200

    Add files via upload
    Dodanie komentarzy do poszczeglnych metod

--- src/miasto.cpp
+++ src/miasto.cpp
@@ -4,11 +4,13 @@ Miasto::Miasto(std::string _nazwaMiasta)
   : liczbaMieszkancow(0), procent(0), nazwaMiasta(_nazwaMiasta), zadowolenieMiasta(0){
   }
 
+// Konstruktor miasta, przypisanie atrybutow, oraz wstepne zaludnienie (wypelnienie mieszkancami) miasta
 Miasto::Miasto(int _N, int _p, std::string _nazwaMiasta)
   : liczbaMieszkancow(_N), procent(_p), nazwaMiasta(_nazwaMiasta), zadowolenieMiasta(0){
   zaludnijMiasto();
 }
-
+// losuje pozycje kibicow i dzieli ich na dwie grupy:
+// czerwonych i zielonych
 void Miasto::zaludnijMiasto(){
   int liczbaCzerwonych = this->procent/100 * this->liczbaMieszkancow;
   int liczbaZielonych = this->liczbaMieszkancow - liczbaCzerwonych;
@@ -20,25 +22,34 @@ void Miasto::zaludnijMiasto(){
     this->kibice.push_back(Kibic('z'));
 }
 
+// Zwraca pozycje poszczegolnych kibicow
 void Miasto::getPozycjeKibicow(){
   for(unsigned int i = 0; i < this->kibice.size(); i++)
     std::cout << kibice[i].getX() << "\t" << kibice[i].getY() << "\t" << kibice[i].getKolor() << std::endl;
 }
 
+// Sprawdza, czy kibice znajdują się odpowiednio blisko siebie, a także
+// decyduje czy w związku z tym jego zadowolenie maleje, rośnie,
+// czy nie zmienia się 
 void Miasto::wyznaczZadowKibica(Kibic& k1, Kibic& k2){
   double k1Cord[] = {k1.getX(), k1.getY()};
 
   double k2Cord[] = {k2.getX(), k2.getY()};
-
+  // Wyznaczenie odlgelosci pomiedzy  kibicami
   double dystans = pow(k2Cord[0] - k1Cord[0], 2) - pow(k2Cord[1] - k1Cord[1], 2);
-  
-  if(dystans <= 1 && k1.getKolor() == k2.getKolor())
+  // Sprawdzanie odleglosci pomiedzy kibicami i decyzja o zwiekszeniu/zmniejszeniu poziomu zadowolenia
+  if(dystans <= 1 && k1.getKolor() == k2.getKolor()){
     k1.zwiekszZadowolenie();
+    k2.zwiekszZadowolenie();
+  }
   
-  else if (dystans <= 1 && k1.getKolor() != k2.getKolor())
+  else if (dystans <= 1 && k1.getKolor() != k2.getKolor()){
     k1.zmniejszZadowolenie();
+    k2.zmniejszZadowolenie();
+  }
 }
 
+// Przebiega po wszystkich parach kibicow wyznaczajac zadowolenie kazdego z nich
 void Miasto::wyznaczCalkZadowKibicow(){
   for(int i = 0; i < this->liczbaMieszkancow; i++){
     for(int j = i+1; j < this->liczbaMieszkancow; j++){
@@ -47,11 +58,12 @@ void Miasto::wyznaczCalkZadowKibicow(){
   }
 }
 
+// Sumuje poziomy zadowolenia kibicow, a kolejno wyznacza srednia arytmetyczna
+// jako wskaznik ogolnego zadowolenia miasta
 void Miasto::wyznaczZadowMiasta(){
   
   if(this->zadowolenieMiasta != 0)
     this->zadowolenieMiasta = 0;
-
 
   for(int i = 0; i < this->liczbaMieszkancow; i++){
     this->zadowolenieMiasta += kibice[i].getZadowolenie();
@@ -59,12 +71,17 @@ void Miasto::wyznaczZadowMiasta(){
   this->zadowolenieMiasta = this->zadowolenieMiasta / this->liczbaMieszkancow;
 }
 
+// Zwraca zadowolenie miasta
 int Miasto::getZadowolenieMiasta(){
   return this->zadowolenieMiasta;
 }
 
+// Przeprowadza ewolucje miasta
 void Miasto::ewolucja(){
   for(int i = 0; i < this->liczbaMieszkancow; i++){
+    // Sprawdza, czy zadowolenie kibica jest co najwyzej 0.
+    // W takim przypadku losuje kat wzgledem OX. Na jego bazie w odpowiednim
+    // kierunku zostaje przesunieta pozycja kibica (promien przesuniecia - 1)
     if(this->kibice[i].getZadowolenie() <= 0){
       double kierunek = 2 * rand()/RAND_MAX * M_PI;
       this->kibice[i].zmienX(cos(kierunek));
@@ -75,6 +92,7 @@ void Miasto::ewolucja(){
   }
 }
 
+// Przywraca zadowolenie kibicow do wartosci domyslnej - 0
 void Miasto::resetZadowKibicow(){
   for(int i = 0; i < this->liczbaMieszkancow; i++){
     this->kibice[i].resetZadow();
