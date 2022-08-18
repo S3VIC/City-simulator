f1ed593c75afdb8c8715161127586352888f9859
S3VIC <62557969+S3VIC@users.noreply.github.com>
2022-08-18 14:40:43 +0200

    Add files via upload
    Dodanie komentarzy do metod

--- src/kibic.cpp
+++ src/kibic.cpp
@@ -2,7 +2,8 @@
 
 Kibic::Kibic()
 {}
-
+// Konstruktor kibica, losowanie koordynatow, ustawianie poziomu zadowolenia na wartosc
+// domyslna
 Kibic::Kibic(const char _kolor)
   : kolor(_kolor)
 {
@@ -11,48 +12,60 @@ Kibic::Kibic(const char _kolor)
   this->zadowolenie = 0;
 }
 
+// zwraca oznaczenie kibica (przynaleznosc do jednej z 2 grup)
 const char Kibic::getKolor(){
   return this->kolor;
 }
 
+// zwraca koordynat X
 const double Kibic::getX(){
   return this->X;
 }
 
+// zwraca koordynat Y
 const double Kibic::getY(){
   return this->Y;
 }
 
+// inkrementacja poziomu zadowolenia kibica
 void Kibic::zwiekszZadowolenie(){
   this->zadowolenie ++;
 }
 
+// dekrementacja poziomu zadowolenia kibica
 void Kibic::zmniejszZadowolenie(){
   this->zadowolenie --;
 }
 
+// zwraca wartosc poziomu zadowolenia kibica
 int Kibic::getZadowolenie(){
   return this->zadowolenie;
 }
 
+// ustawia koordynat X
 void Kibic::setX(double _X){
   this->X = _X;
 }
 
+// ustawia koordynat Y
 void Kibic::setY(double _Y){
   this->Y = _Y;
 }
 
+// zmienia koordynat X o okreslona wartosc (podczas ewolucji miasta)
 void Kibic::zmienX(double _X){
   this->X = this->X + _X;
 }
 
+// zmienia koordynat Y o okreslona wartosc (podczas ewolucji miasta)
 void Kibic::zmienY(double _Y){
   this->Y = this->Y + _Y;
 }
 
+// przywraca zadowolenie kibica do wartosci domyslnej
 void Kibic::resetZadow(){
   this->zadowolenie = 0;
 }
+
 Kibic::~Kibic()
 {}
